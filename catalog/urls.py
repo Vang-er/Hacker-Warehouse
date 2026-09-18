91fe86abdd0e6e492e739c57c381abbded986d49
Timothy <timo.dev96@gmail.com>
2026-09-18 12:26:35 +0300

    Products and product variant APIs

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CategoryViewSet
+from .views import CategoryViewSet , ProductViewSet, VariantViewSet
 
 router = DefaultRouter()
 router.register("categories", CategoryViewSet, basename="category")
+router.register("products", ProductViewSet, basename="product")
+router.register("variants", VariantViewSet, basename="variant")
 
 urlpatterns = [path("", include(router.urls))]
