c6ee127fa3b2292278801f92e78fa878b8fc53ef
baraa <baraatv2006@gmail.com>
2026-09-18 23:39:26 +0300

    pages

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 const stock = document.getElementById("your")
 const add = document.getElementById("add")
 const dashboard = document.getElementById("dash")
+const stockdiv = document.getElementById("stockdiv")
+const adddiv = document.getElementById("adddiv")
+const dashdiv = document.getElementById("dashdiv")
 let butnum = 0;
+
+
+
+
+
+
+
+
+
+
+
+
 function chosestock(){
 stock.style.backgroundColor = "#78cc78"
 stock.style.borderRadius = "20px"
@@ -9,6 +24,9 @@ add.style.backgroundColor = "#fff"
 add.style.borderRadius = "0px"
 dashboard.style.backgroundColor = "#fff"
 dashboard.style.borderRadius = "0px"
+stockdiv.style.display = "block"
+adddiv.style.display = "none"
+dashdiv.style.display = "none"
 }
 function choseadd(){
 add.style.backgroundColor = "#78cc78"
@@ -17,6 +35,9 @@ stock.style.backgroundColor = "#fff"
 stock.style.borderRadius = "0px"
 dashboard.style.backgroundColor = "#fff"
 dashboard.style.borderRadius = "0px"
+stockdiv.style.display = "none"
+adddiv.style.display = "block"
+dashdiv.style.display = "none"
 }
 function chosedash(){
 dashboard.style.backgroundColor = "#78cc78"
@@ -25,4 +46,7 @@ add.style.backgroundColor = "#fff"
 add.style.borderRadius = "0px"
 stock.style.backgroundColor = "#fff"
 stock.style.borderRadius = "0px"
+stockdiv.style.display = "none"
+adddiv.style.display = "none"
+dashdiv.style.display = "block"
 }
