8aaa63edcf22bf72ae337283e179384a787779f5
baraa <baraatv2006@gmail.com>
2026-09-18 22:07:02 +0300

    some effects on click

---
+++ main.js
@@ -0,0 +1,28 @@
+const stock = document.getElementById("your")
+const add = document.getElementById("add")
+const dashboard = document.getElementById("dash")
+let butnum = 0;
+function chosestock(){
+stock.style.backgroundColor = "#78cc78"
+stock.style.borderRadius = "20px"
+add.style.backgroundColor = "#fff"
+add.style.borderRadius = "0px"
+dashboard.style.backgroundColor = "#fff"
+dashboard.style.borderRadius = "0px"
+}
+function choseadd(){
+add.style.backgroundColor = "#78cc78"
+add.style.borderRadius = "20px"
+stock.style.backgroundColor = "#fff"
+stock.style.borderRadius = "0px"
+dashboard.style.backgroundColor = "#fff"
+dashboard.style.borderRadius = "0px"
+}
+function chosedash(){
+dashboard.style.backgroundColor = "#78cc78"
+dashboard.style.borderRadius = "20px"
+add.style.backgroundColor = "#fff"
+add.style.borderRadius = "0px"
+stock.style.backgroundColor = "#fff"
+stock.style.borderRadius = "0px"
+}
